597c49573ceecb334435f6869905e80cb44b07a4
djh-sudo <66478183+djh-sudo@users.noreply.github.com>
2021-07-23 22:41:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import allPeople
 from pyecharts.charts import Calendar,Page
 
 
-Page.save_resize_html("render.html",cfg_file="chart_config.json")
+# Page.save_resize_html("render.html",cfg_file="chart_config.json")
 
-# def handleCalendar():
-#     page = Page(layout=Page.DraggablePageLayout)
-#     page.add(
-#     date.calendar(),
-#     stackBar.get_year_overlap_chart(),
-#     byClass.get_year_overlap_chart(),
-#     allPeople.getStructure(),
-#     )
-#     page.render("render.html")
-#
-#
-# handleCalendar()
+ def handleCalendar():
+     page = Page(layout=Page.DraggablePageLayout)
+     page.add(
+     date.calendar(),
+     stackBar.get_year_overlap_chart(),
+     byClass.get_year_overlap_chart(),
+     allPeople.getStructure(),
+     )
+     page.render("render.html")
+
+
+ handleCalendar()
